9d4b018929680eac52553624b47a2414151900e5
neverset <yuwei.wang@mercedes-benz.com>
2025-03-23 15:48:46 +0100

    add find match

--- script/publisher.py
+++ script/publisher.py
@@ -56,17 +56,18 @@ def find_match(df):
     db = lancedb.connect("./lancedb")
     table_name = "property_listing"
     table = db.open_table(table_name)
-    # preference_text = " ".join(df["user_input"].tolist())
-    preference_text = "Herzlich willkommen in Fellbach-Schmiden. \n\nIhr neues Zuhause befindet sich am Ortsrand in einem ruhigen und sonnigen Wohngebiet. Sie wohnen und leben mit hohem Naherholungswert nahezu im Grünen in der Nähe des Schmidener Felds.\n\nDie Ortsmitte von Fellbach-Schmiden erreichen Sie zu Fuß oder mit dem Auto in wenigen Minuten. Dort finden Sie zusätzlich zu den Geschäften für den täglichen Bedarf Arztpraxen, Apotheken und weitere Gesundheitsdienstleister"
+    preference_text = " ".join(df["user_input"].tolist())
     preference_embedded = embed_func(preference_text)[0]
-    df_matched = table.search(preference_embedded).metric("cosine").limit(3).to_pandas()
+    df_matched = table.search(preference_embedded).metric("cosine").limit(5).where("description!=''", prefilter=True).to_pandas()
     return df_matched
 
+
+
 if __name__ == "__main__":
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', None)
-    pd.set_option('display.max_colwidth', None)
+    # pd.set_option('display.max_rows', None)
+    # pd.set_option('display.max_columns', None)
+    # pd.set_option('display.width', None)
+    # pd.set_option('display.max_colwidth', None)
     db_name = "./lancedb"
     load_dotenv()
     NOTION_TOKEN = os.getenv("NOTION_TOKEN")
